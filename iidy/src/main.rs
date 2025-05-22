938143eabd5d63ae66aad5d2c2a9933f9a46675d
Tavis Rudd <tavis@damnsimple.com>
2025-05-22 09:08:51 -0700

    Add initial rust demo command

--- iidy/src/main.rs
+++ iidy/src/main.rs
@@ -18,6 +18,7 @@ mod cfn {
 }
 use clap::{CommandFactory, Parser};
 use clap_complete::{Shell, generate};
+mod demo;
 use cli::{Cli, Commands};
 use tokio::runtime::Runtime;
 
@@ -79,7 +80,11 @@ fn main() {
         Commands::DummySpacer5 => {}
         Commands::Render(args) => println!("render {:?}", args),
         Commands::GetImport(args) => println!("get-import {:?}", args),
-        Commands::Demo(args) => println!("demo {:?}", args),
+        Commands::Demo(args) => {
+            if let Err(e) = rt.block_on(demo::run(&args.demoscript, args.timescaling)) {
+                eprintln!("demo failed: {e:?}");
+            }
+        }
         Commands::LintTemplate(args) => println!("lint-template {:?}", args),
         Commands::ConvertStackToIidy(args) => println!("convert-stack-to-iidy {:?}", args),
         Commands::InitStackArgs(args) => println!("init-stack-args {:?}", args),
